12376612e8f25e672bfd2637ca53755efe9b0b95
mertkantaroglu <mertkantaroglu@gmail.com>
2023-08-12 18:33:14 +0300

    Create gifts migration

---
+++ db/migrate/20230812152830_create_gifts.rb
@@ -0,0 +1,15 @@
+class CreateGifts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :gifts do |t|
+      t.string :name, null: false
+      t.decimal :price, null: false
+      t.string :color, null: false
+      t.integer :count, null: false
+      t.string :image_url, null: false
+      t.string :description, null: false
+      t.string :specs, null:false
+
+      t.timestamps
+    end
+  end
+end
