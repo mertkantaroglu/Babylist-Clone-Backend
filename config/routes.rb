906fb8fdede22676fe708a48f88b1cee648c1b3b
mertkantaroglu <mertkantaroglu@gmail.com>
2023-08-15 19:54:07 +0300

    Update gift routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:index, :show, :create]
+      resources :gifts, only: [:index, :show, :create, :new, :destroy]
     end
   end
 end
