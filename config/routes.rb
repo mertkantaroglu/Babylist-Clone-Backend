324b321c70fcc3e4145fa4db2ae29dc03ee48505
mertkantaroglu <mertkantaroglu@gmail.com>
2023-08-16 19:07:36 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:index, :show, :create]
+      resources :users, only: [:index, :show, :create] do
+        resources :gifts, only: [:index, :show, :create, :new, :destroy]
+      end
+
       resources :gifts, only: [:index, :show, :create, :new, :destroy]
     end
   end
