373f07416b07d8d3b9a5891ed45b936dd10c9d49
mertkantaroglu <mertkantaroglu@gmail.com>
2023-08-18 18:57:42 +0300

    Add create function

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -3,4 +3,19 @@ class Api::V1::GiftsController < ApplicationController
     # return gifts in descending order
     render json: Gift.all.order(created_at: :desc)
   end
+
+  def create
+    gift = Gift.create!(gift_params)
+    if gift
+      render json: gift
+    else
+      render json: gift.errors
+    end
+  end
+
+  private
+
+  def gift_params
+    params.permit(:name, :price, :color, :count, :image_url, :description, :specs)
+  end
 end
