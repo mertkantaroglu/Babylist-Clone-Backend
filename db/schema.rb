33e4306ee99381f568899cdcb9c9a202564d16a4
mertkantaroglu <mertkantaroglu@gmail.com>
2023-08-30 20:09:55 +0300

    Update schema file

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_12_152830) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_30_164055) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cart_items", force: :cascade do |t|
+    t.string "name"
+    t.integer "price"
+    t.integer "count"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_cart_items_on_user_id"
+  end
 
   create_table "gifts", force: :cascade do |t|
     t.string "name", null: false
@@ -35,5 +45,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_12_152830) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "cart_items", "users"
   add_foreign_key "gifts", "users"
 end
