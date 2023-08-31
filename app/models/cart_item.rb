27c03eba522d05fe2e5ac0230010a627446013c6
mertkantaroglu <mertkantaroglu@gmail.com>
2023-08-31 20:13:04 +0300

    Update cartitem

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,5 +1,8 @@
 class CartItem < ApplicationRecord
   belongs_to :user, class_name: 'User'
 
+  has_many :gift_cartitem, dependent: :destroy
+  has_many :gifts, through: :gift_cartitem, dependent: :destroy
+
   validates :name, :price, :count, presence: true
 end
