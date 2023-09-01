6851921d48642ba8c6a6969a3759f152a91a1d38
mertkantaroglu <mertkantaroglu@gmail.com>
2023-09-01 21:21:06 +0300

    Update schema file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_30_164055) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_01_181104) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,9 +19,20 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_30_164055) do
     t.integer "price"
     t.integer "count"
     t.bigint "user_id", null: false
+    t.bigint "gift_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["gift_id"], name: "index_cart_items_on_gift_id"
     t.index ["user_id"], name: "index_cart_items_on_user_id"
+  end
+
+  create_table "gift_cartitems", force: :cascade do |t|
+    t.bigint "gift_id", null: false
+    t.bigint "cartitem_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cartitem_id"], name: "index_gift_cartitems_on_cartitem_id"
+    t.index ["gift_id"], name: "index_gift_cartitems_on_gift_id"
   end
 
   create_table "gifts", force: :cascade do |t|
@@ -45,6 +56,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_30_164055) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "cart_items", "gifts"
   add_foreign_key "cart_items", "users"
+  add_foreign_key "gift_cartitems", "cart_items", column: "cartitem_id"
+  add_foreign_key "gift_cartitems", "gifts"
   add_foreign_key "gifts", "users"
 end
