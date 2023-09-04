f60dfc9832a14fc86c9cbd57fff77ad05ccfe55b
mertkantaroglu <mertkantaroglu@gmail.com>
2023-09-04 20:09:42 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,14 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: [:index, :show, :create] do
         resources :gifts, only: [:index, :show, :create, :new, :destroy]
+        resources :cart_items, only: [:index, :show, :create, :destroy]
       end
 
-      resources :gifts, only: [:index, :show, :create, :new, :destroy]
+      resources :gifts, only: [:index, :show, :create, :new, :destroy] do
+        resources :cart_items, only: [:index, :show, :create, :destroy]
+      end
     end
+
+      # resources :gifts,
   end
 end
